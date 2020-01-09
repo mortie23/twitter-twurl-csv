300846f361f107863570ccaec2b74092143b84c0
christopher <christopher@email.com>
2020-01-10 09:42:28 +1100

    replace tab with space

--- common-functions.sh
+++ common-functions.sh
@@ -1,52 +1,52 @@
-## Name:	Common Functions
-## Author:	Christopher Moirtimer
-## Date:	2019-05-20
-## Desc:	Standard functions for use across all scripts
-## Usage:	. commonFunctions.sh
+## Name:  Common Functions
+## Author:  Christopher Moirtimer
+## Date:  2019-05-20
+## Desc:  Standard functions for use across all scripts
+## Usage:  . commonFunctions.sh
 
 ## Echo a info log level message with the current time
 ## usage echoLog "<loglevel>" "<message>"
 ## where log level is [INFO,WARN,ERROR,etc]
 logfile=./data/logs/log-`date +'%Y%m%d_%H%M%S'.log`
 function echoLog() {
-	logLevel=${1}
-	message=${2}
-	strdtm=`date +'%Y-%m-%d_%H:%M:%S'`
-	echo -e "${logLevel}, ${strdtm}, func(${FUNCNAME[1]}), ${message}" |& tee -a ${logfile}
+  logLevel=${1}
+  message=${2}
+  strdtm=`date +'%Y-%m-%d_%H:%M:%S'`
+  echo -e "${logLevel}, ${strdtm}, func(${FUNCNAME[1]}), ${message}" |& tee -a ${logfile}
 }
 
 ## to parse arguments
 function parseArgs() {
-	POSITIONAL=()
-	while [[ $# -gt 0 ]]
-	do
-	key="$1"
+  POSITIONAL=()
+  while [[ $# -gt 0 ]]
+  do
+  key="$1"
 
-	case $key in
-		-s|--search_string)
-		search_string="$2"
-		shift # past argument
-		shift # past value
-		;;
-		-n|--num_files)
-		num_files="$2"
-		shift # past argument
-		shift # past value
-		;;
-		-st|--search_type)
-		search_type="$2"
-		shift # past argument
-		shift # past value
-		;;
-		*)    # unknown option
-		POSITIONAL+=("$1") # save it in an array for later
-		shift # past argument
-		;;
-	esac
-	done
-	set -- "${POSITIONAL[@]}" # restore positional parameters
+  case $key in
+    -s|--search_string)
+    search_string="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--num_files)
+    num_files="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -st|--search_type)
+    search_type="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+  esac
+  done
+  set -- "${POSITIONAL[@]}" # restore positional parameters
 
-	echo search_string = "${search_string}"
-	echo num_files = "${num_files}"
-	echo search_type = "${search_type}"
+  echo search_string = "${search_string}"
+  echo num_files = "${num_files}"
+  echo search_type = "${search_type}"
 }
--- twurl-status-lean.sh
+++ twurl-status-lean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-## Name:	Christopher Mortimer
-## Date:	2020-01-09
-## Desc:	Get all the statuses for a search term
-## Usage:	./twurl-status.sh -s <search_string> -n <num_files>
-##				example:
-##				./twurl-status-lean.sh -s mortie23 -n 100
+## Name:  Christopher Mortimer
+## Date:  2020-01-09
+## Desc:  Get all the statuses for a search term
+## Usage:  ./twurl-status.sh -s <search_string> -n <num_files>
+##        example:
+##        ./twurl-status-lean.sh -s mortie23 -n 100
 
 . common-functions.sh
 parseArgs "$@"
@@ -17,48 +17,48 @@ next_results=null
 
 ## Call the Twitter status API
 function APICall() {
-	echoLog "INFO" "\e[34mSTART\e[0m, search: ${search_string}, max_id: ${max_id}, since_id: ${since_id}"
+  echoLog "INFO" "\e[34mSTART\e[0m, search: ${search_string}, max_id: ${max_id}, since_id: ${since_id}"
 
-	## Loop through the number of times the user inputs
-	for i in $(eval echo {1..${num_files}}); do
-		## on first run dont send an id
-		if [ ${i} == 1 ]; then
-			twurl "/1.1/search/tweets.json?q=${search_string}&result_type=recent&count=100&lang=en&tweet_mode=extended" > ./data/${search_string}-${i}.json
-		else
-			twurl "/1.1/search/tweets.json${next_results}" > ./data/${search_string}-${i}.json
-		fi
+  ## Loop through the number of times the user inputs
+  for i in $(eval echo {1..${num_files}}); do
+    ## on first run dont send an id
+    if [ ${i} == 1 ]; then
+      twurl "/1.1/search/tweets.json?q=${search_string}&result_type=recent&count=100&lang=en&tweet_mode=extended" > ./data/${search_string}-${i}.json
+    else
+      twurl "/1.1/search/tweets.json${next_results}" > ./data/${search_string}-${i}.json
+    fi
 
-		## Remove all newlines
-		sed -i 's/\\n/ /g' ./data/${search_string}-${i}.json
-		sed -i 's/\\r/ /g' ./data/${search_string}-${i}.json
+    ## Remove all newlines
+    sed -i 's/\\n/ /g' ./data/${search_string}-${i}.json
+    sed -i 's/\\r/ /g' ./data/${search_string}-${i}.json
 
-		## parsing the search parameters
-		max_id=`jq '.search_metadata.max_id' ./data/${search_string}-${i}.json`
-		since_id=`jq '.search_metadata.since_id' ./data/${search_string}-${i}.json`
-		count=`jq '.search_metadata.count' ./data/${search_string}-${i}.json`
+    ## parsing the search parameters
+    max_id=`jq '.search_metadata.max_id' ./data/${search_string}-${i}.json`
+    since_id=`jq '.search_metadata.since_id' ./data/${search_string}-${i}.json`
+    count=`jq '.search_metadata.count' ./data/${search_string}-${i}.json`
 
-		## finding the last id, to send it to the next request
-		## send list of all status id to temp file
-		jq '.statuses[].id' ./data/${search_string}-${i}.json > temp.txt
-		## create an array of all the status id
-		IFS=$'\n' read -d '' -r -a lines < temp.txt
-		## find the length of the statuses
-		len=`cat temp.txt | wc -l`
-		echoLog "INFO" "\e[93m${i} of ${num_files}\e[0m, \e[93mlen: ${len}\e[0m, count: ${count}, max_id: ${max_id}, since_id: ${since_id}"
-		rm temp.txt
+    ## finding the last id, to send it to the next request
+    ## send list of all status id to temp file
+    jq '.statuses[].id' ./data/${search_string}-${i}.json > temp.txt
+    ## create an array of all the status id
+    IFS=$'\n' read -d '' -r -a lines < temp.txt
+    ## find the length of the statuses
+    len=`cat temp.txt | wc -l`
+    echoLog "INFO" "\e[93m${i} of ${num_files}\e[0m, \e[93mlen: ${len}\e[0m, count: ${count}, max_id: ${max_id}, since_id: ${since_id}"
+    rm temp.txt
 
-		## find the next results. somthing strange with adding a %23
-		next_results=`jq -r '.search_metadata.next_results' ./data/${search_string}-${i}.json`
-		#next_results=`echo "${next_results_tmp}" | sed "s/%25/%/g"`
-		#echoLog "INFO" "next_results: ${next_results}"
+    ## find the next results. somthing strange with adding a %23
+    next_results=`jq -r '.search_metadata.next_results' ./data/${search_string}-${i}.json`
+    #next_results=`echo "${next_results_tmp}" | sed "s/%25/%/g"`
+    #echoLog "INFO" "next_results: ${next_results}"
 
-		if [ ${next_results} == "null" ]; then
-			echoLog "INFO" "No more results"
-			exit
-		fi
+    if [ ${next_results} == "null" ]; then
+      echoLog "INFO" "No more results"
+      exit
+    fi
 
-	done
-	echoLog "INFO" "\e[31mEND\e[0m, search: ${search_string}"
+  done
+  echoLog "INFO" "\e[31mEND\e[0m, search: ${search_string}"
 }
 
 ## Call the functions
